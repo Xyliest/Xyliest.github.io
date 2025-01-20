fb6f3a11ef9a1753facb7f3f823c4df5ccc273f0
Xyliest <cubifinity@gmail.com>
2025-01-20 14:54:22 -0500

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,19 @@ function adjust(el,pN) {
 	pP=pP.getBoundingClientRect();
 	elW=pP["width"]-vw/50;
 	elH=pP["width"]-vw/50;
+	el.width=elW;
+	el.height=elH;
+	el.style.visibility="visible";
+}
+
+function adjustSVG(el,pN) {
+	pP=el;
+	for(i=0;i<pN;i++) {
+		pP=pP.parentElement;
+	}
+	pP=pP.getBoundingClientRect();
+	elW=pP["width"]-vw/50;
+	elH=pP["width"]-vw/50;
 	el.width.baseVal.value=elW;
 	el.height.baseVal.value=elH;
 	el.style.visibility="visible";
